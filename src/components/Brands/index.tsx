d1eb5d21d0dbd7e512d0b6170041c4ff1ee27004
Ronald Arifin <ronaldarifin@berkeley.edu>
2024-08-21 18:36:41 -0700

    skills svgs, blogs baseline

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,11 +1,17 @@
 import { Brand } from "@/types/brand";
 import Image from "next/image";
 import brandsData from "./brandsData";
+import SectionTitle from "../Common/SectionTitle";
 
 const Brands = () => {
   return (
-    <section className="pt-16">
+    <section className="pt-4 pb-16">
       <div className="container">
+        <SectionTitle
+            title="Skills"
+            paragraph="Some skills I gained over the years."
+            center
+          />
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
             <div className="flex flex-wrap items-center justify-center rounded-sm bg-gray-light px-8 py-8 dark:bg-gray-dark sm:px-10 md:px-[50px] md:py-[40px] xl:p-[50px] 2xl:px-[70px] 2xl:py-[60px]">
@@ -31,7 +37,7 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
         href={href}
         target="_blank"
         rel="nofollow noreferrer"
-        className="relative h-10 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
+        className="relative h-20 w-full opacity-70 transition hover:opacity-100 dark:opacity-60 dark:hover:opacity-100"
       >
         <Image src={imageLight} alt={name} fill className="hidden dark:block" />
         <Image src={image} alt={name} fill className="block dark:hidden" />
