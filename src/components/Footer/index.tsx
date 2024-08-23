2d36d6ef7812fcb5b95857db8175cacb60e1b6c3
Ronald Arifin <ronaldarifin@berkeley.edu>
2024-08-23 14:29:28 -0700

    fixed footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -30,7 +30,7 @@ const Footer = () => {
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                   Integer lobortis.
                 </p> */}
-                <div className="flex items-center">
+                {/* <div className="flex items-center">
                   <a
                     href="/"
                     aria-label="social-link"
@@ -105,7 +105,7 @@ const Footer = () => {
                       <path d="M15.2196 0H1.99991C1.37516 0 0.875366 0.497491 0.875366 1.11936V14.3029C0.875366 14.8999 1.37516 15.4222 1.99991 15.4222H15.1696C15.7943 15.4222 16.2941 14.9247 16.2941 14.3029V1.09448C16.3441 0.497491 15.8443 0 15.2196 0ZM5.44852 13.1089H3.17444V5.7709H5.44852V13.1089ZM4.29899 4.75104C3.54929 4.75104 2.97452 4.15405 2.97452 3.43269C2.97452 2.71133 3.57428 2.11434 4.29899 2.11434C5.02369 2.11434 5.62345 2.71133 5.62345 3.43269C5.62345 4.15405 5.07367 4.75104 4.29899 4.75104ZM14.07 13.1089H11.796V9.55183C11.796 8.7061 11.771 7.58674 10.5964 7.58674C9.39693 7.58674 9.222 8.53198 9.222 9.47721V13.1089H6.94792V5.7709H9.17202V6.79076H9.19701C9.52188 6.19377 10.2466 5.59678 11.3711 5.59678C13.6952 5.59678 14.12 7.08925 14.12 9.12897V13.1089H14.07Z" />
                     </svg>
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
 
@@ -117,7 +117,9 @@ const Footer = () => {
                 <ul>
                   <li>
                     <Link
-                      href="/blog"
+                      href="https://drive.google.com/drive/folders/11ha7bY5-gwIbpMbGr7zlVeJr06UKBF_1?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
                       Teaching Slides
@@ -125,18 +127,20 @@ const Footer = () => {
                   </li>
                   <li>
                     <Link
-                      href="/"
+                      href="https://github.com/ronaldarifin"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Pricing
+                      Github
                     </Link>
                   </li>
                   <li>
                     <Link
-                      href="/about"
+                      href="https://leetcode.com/u/ronaldarifin/"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      About
+                      Leetcode
                     </Link>
                   </li>
                 </ul>
@@ -180,31 +184,35 @@ const Footer = () => {
             <div className="w-full px-4 md:w-1/2 lg:w-4/12 xl:w-3/12">
               <div className="mb-12 lg:mb-16">
                 <h2 className="mb-10 text-xl font-bold text-black dark:text-white">
-                  Support & Help
+                  Socials
                 </h2>
                 <ul>
                   <li>
                     <Link
-                      href="/contact"
+                      href="mailto:your-email@example.com?subject=Let's Connect&body=Hi Ronald,"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Open Support Ticket
+                      Email
                     </Link>
                   </li>
                   <li>
                     <Link
-                      href="/"
+                      href="https://www.linkedin.com/in/ronaldarifin/"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      Terms of Use
+                      LinkedIn
                     </Link>
                   </li>
                   <li>
                     <Link
-                      href="/about"
+                      href="https://devpost.com/ronaldarifin"
+                      target="_blank"
+                      rel="noopener noreferrer"
                       className="mb-4 inline-block text-base text-body-color duration-300 hover:text-primary dark:text-body-color-dark dark:hover:text-primary"
                     >
-                      About
+                      Devpost
                     </Link>
                   </li>
                 </ul>
