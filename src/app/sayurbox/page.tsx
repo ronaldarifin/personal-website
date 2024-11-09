dcecec6be10d921e6a5fb1e18ba6149821685fc5
Ronald Arifin <ronaldarifin@berkeley.edu>
2024-11-09 01:42:13 -0800

    fix build error

--- src/app/sayurbox/page.tsx
+++ src/app/sayurbox/page.tsx
@@ -1,1 +1,12 @@
- 
+export default function HrHouzPage() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-gray-dark">
+      <div className="container mx-auto px-4 py-20">
+        <h1 className="mb-6 text-3xl text-gray-800 dark:text-gray-200">
+          My Experience at HrHouz
+        </h1>
+        <div className="text-lg text-gray-600 dark:text-gray-400">During</div>
+      </div>
+    </div>
+  );
+}
